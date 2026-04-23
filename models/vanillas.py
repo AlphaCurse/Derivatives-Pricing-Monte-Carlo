6beec1cebf5f7bb243bfb257f451630f25e59598
Tyler Brown, MBA, MSDS <tyler.xavier.brown@gmail.com>
2026-04-23 08:41:56 -0400

    Update vanillas.py

--- models/vanillas.py
+++ models/vanillas.py
@@ -6,13 +6,11 @@ class VanillaPricer:
         self.T = T
 
     def call_payoff(self, K, paths):
-        """Standard European Call: max(S_T - K, 0)"""
         terminal_prices = paths[-1]
         payoffs = np.maximum(terminal_prices - K, 0)
         return np.exp(-self.r * self.T) * np.mean(payoffs)
 
     def put_payoff(self, K, paths):
-        """Standard European Put: max(K - S_T, 0)"""
         terminal_prices = paths[-1]
         payoffs = np.maximum(K - terminal_prices, 0)
         return np.exp(-self.r * self.T) * np.mean(payoffs)
